f9b35ae02b07717b88434132d719e39332e92cea
AlexPer <alexper@list.ru>
2024-01-29 16:53:03 +0300

    Работа по замечаниям

--- seminar3/main.swift
+++ seminar3/main.swift
@@ -5,10 +5,25 @@
 //   покупатель (необходимо вывести его номер по порядку, чай, который
 //   он заказал, и стоимость).
 print("Task 1 -=-=-=-=-=-=-=-=-=-=-=-=-")
-var teaArray: [String: Int] = ["Greeen tea": 100, "Black tea": 70, "EarlGray": 80, "Fruit tea": 110]
-var bayerArray = ["Greeen tea", "Fruit tea", "EarlGray", "Black tea"]
-for (id, tea) in bayerArray.enumerated(){
-    print("№",id+1,tea,teaArray[tea]!)
+enum Tea: String {
+    case green = "Greeen tea"
+    case black = "Black tea"
+    case earlgray = "EarlGray"
+    case fruit = "Fruit tea"
+    }
+
+func getCost (tea: Tea) -> Double {
+    switch tea {
+    case .green: return 90.0
+    case .black: return 70.0
+    case .earlgray: return 80.0
+    case .fruit: return 100.0
+    }
+}
+var bayedTea = [Tea.black,Tea.fruit,Tea.earlgray,Tea.black,Tea.green,Tea.fruit]
+
+for (id, bayTea) in bayedTea.enumerated() {
+    print("№",id + 1,bayTea.rawValue, "\t- ", getCost(tea: bayTea))
 }
 
 //2. Есть массив [-4, 5, 10, nil, 4, nil, 25, 0, nil, 16, 75, -20, -7, 15, 0, nil].
@@ -17,8 +32,8 @@ for (id, tea) in bayerArray.enumerated(){
 //   отсортирован по возрастанию.
 print("\nTask 2 -=-=-=-=-=-=-=-=-=-=-=-=-")
 var arrayIn = [-4, 5, 10, nil, 4, nil, 25, 0, nil, 16, 75, -20, -7, 15, 0, nil]
-var arrayOut = arrayIn.filter{ $0 != nil && $0 != 0 && $0 != 4 }
-                      .compactMap{ Int ($0!)}
+var arrayOut = arrayIn.compactMap{$0}
+                      .filter{ $0 != 0 && $0 != 4 }
                       .sorted(by: < )
 print(arrayIn)
 print(arrayOut)
@@ -34,7 +49,7 @@ print("\nTask 3 -=-=-=-=-=-=-=-=-=-=-=-=-")
 //}
 func myfunc (a: Int) -> [Int]{
     var array: [Int] = [1]
-    for e in 1...(a-1){
+    for _ in 1...(a-1){
         array.append((array.last!)*2)
     }
     return array
